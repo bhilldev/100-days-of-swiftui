25fc2d72f6b4f420ed7b7a0533d1b0cfad865dfc
Brandon Hill <bhilldev@outlook.com>
2026-06-22 14:47:38 -0500

    Complete Day 21 with my solution

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,15 +8,101 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var userScore = 0
+    @State private var questionNumber = 0
+    
+    @State private var showFinalScore = false
+    
+    let maxQuestion = 8
     
     var body: some View {
-        Button("Show Alert") {
-            showingAlert = true
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                VStack(spacing: 15){
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    ForEach(0..<3) { number in
+                        Button{
+                            // flag was tapped
+                            flagTapped(number)
+                            
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(userScore)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
+            }
+            .padding()
         }
-        .alert("Important message", isPresented: $showingAlert) {
-            Button("OK") { }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(userScore)")
         }
+        .alert("Game Over", isPresented: $showFinalScore) {
+            Button("Play Again", action: resetGame)
+        } message: {
+            Text("Final score: \(userScore) / \(maxQuestion)")
+        }
+    }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            userScore += 1
+        } else {
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
+        }
+        questionNumber += 1
+        showingScore = true
+    }
+    func askQuestion() {
+        if(questionNumber >= maxQuestion) {
+            showFinalScore = true
+            
+        } else {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        }
+        
+    }
+    func resetGame() {
+        userScore = 0
+        questionNumber = 0
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
