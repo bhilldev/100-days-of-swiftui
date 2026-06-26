73e1525924ae4dcee0929efd612f938437222288
Brandon Hill <bhilldev@outlook.com>
2026-06-26 12:57:58 -0500

    Complete Day 27

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Brandon Hill on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
