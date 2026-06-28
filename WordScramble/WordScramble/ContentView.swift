cd787e440051e6a0c67e0a4cc571372de59eba95
Brandon Hill <bhilldev@outlook.com>
2026-06-28 14:45:12 -0500

    Complete Day 29: Working with strings

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Brandon Hill on 6/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+    
+    func testStrings(){
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Brandon Hill on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
