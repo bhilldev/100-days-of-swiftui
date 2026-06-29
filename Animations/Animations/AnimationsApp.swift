94edaae24133b463e2df061dfd487c56c9344952
Brandon Hill <bhilldev@outlook.com>
2026-06-29 11:52:33 -0500

    Complete Day 32

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Brandon Hill on 6/29/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Brandon Hill on 6/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
+    var body: some View {
+        Button("Tap Me") {
+            withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                animationAmount += 360
+            }
+            
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0)
+        )
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
