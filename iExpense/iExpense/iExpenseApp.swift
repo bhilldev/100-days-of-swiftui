9af2b0db0937f0d16ec3b37c7cfb2c08fbae096a
Brandon Hill <bhilldev@outlook.com>
2026-07-03 12:21:16 -0500

    Complete Day 36

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Brandon Hill on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
