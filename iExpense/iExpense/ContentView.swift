7a3275225d65a5e06c60b55f1d5aee94edcfbef0
Brandon Hill <bhilldev@outlook.com>
2026-07-04 14:41:18 -0500

    Complete Day 37

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,21 +7,71 @@
 
 import SwiftUI
 
-struct User: Codable{
-    let firstName: String
-    let lastName: String
+struct ExpenseItem: Identifiable, Codable
+{
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
 }
-struct ContentView: View {
-    @State private var user = User(firstName: "Taylor", lastName: "Swift")
-    var body: some View {
-        Button("Save User") {
-            let encoder = JSONEncoder()
 
-            if let data = try? encoder.encode(user) {
-                UserDefaults.standard.set(data, forKey: "UserData")
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
             }
         }
     }
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+
+        items = []
+    }
+}
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                            }
+
+                            Spacer()
+                            Text(item.amount, format: .currency(code: "USD"))
+                        }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .sheet(isPresented: $showingAddExpense) {
+                // show an AddView here
+                AddView(expenses: expenses)
+            }
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    showingAddExpense = true
+                }
+            }
+        }
+    }
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
 }
 
 #Preview {
