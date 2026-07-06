6b274f75d525ed2208ec1a94556abaf0aa9768ed
Brandon Hill <bhilldev@outlook.com>
2026-07-06 12:18:48 -0500

    Complete Day 39

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Brandon Hill on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
