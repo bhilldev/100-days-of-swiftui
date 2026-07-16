53cbced449b01ded4474ce17da2d0eb0bd8296c7
Brandon Hill <bhilldev@outlook.com>
2026-07-16 12:47:03 -0500

    Complete Day 49: Codable and URLSession, AsyncImage, Validating and Disabling Forms

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Brandon Hill on 7/16/26.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
