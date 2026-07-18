bf48a83fe565a372273dea9c9f97a7f018ab8515
Brandon Hill <bhilldev@outlook.com>
2026-07-18 12:37:13 -0500

    Complete Day 51: Checking for valid address, preparing for checkout, sending and receiving orders over internet

--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -9,12 +9,56 @@ import SwiftUI
 import Observation
 
 @Observable
-class Order {
+class Order: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zip = "zip"
+    }
     
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
 
     var type = 0
     var quantity = 3
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var cost: Decimal {
+        // $2 per cake
+        var cost = Decimal(quantity) * 2
+
+        // complicated cakes cost more
+        cost += Decimal(type) / 2
+
+        // $1/cake for extra frosting
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+
+        // $0.50/cake for sprinkles
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+
+        return cost
+    }
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+
+        return true
+    }
 
     var specialRequestEnabled = false {
         didSet {
