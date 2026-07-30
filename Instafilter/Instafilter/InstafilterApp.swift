06170dbcb6698bb548d4a4979c2a3cf10dd90447
Brandon Hill <bhilldev@outlook.com>
2026-07-30 14:48:19 -0500

    Complete Day 62: Showing multiple options with confirmationDialog()

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Brandon Hill on 7/30/26.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
