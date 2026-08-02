8c83999d05797aceefe738901c3f55e3c9843ee0
Brandon Hill <bhilldev@outlook.com>
2026-08-02 13:46:02 -0500

    Complete Day 65

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -7,39 +7,75 @@
 
 import PhotosUI
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
-    @State private var pickerItem: PhotosPickerItem?
-    @State private var selectedImage: Image?
-    @State private var pickerItems = [PhotosPickerItem]()
-    @State private var selectedImages = [Image]()
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
+    func changeFilter() {
+        
+    }
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
+    }
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
+        }
+    }
     
     var body: some View {
-        VStack {
-            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)])) {
-                Label("Select a picture", systemImage: "photo")
-            }
-            selectedImage?
-                .resizable()
-                .scaledToFit()
-            ScrollView {
-                ForEach(0..<selectedImages.count, id: \.self) { i in
-                    selectedImages[i]
-                        .resizable()
-                        .scaledToFit()
-                }
-            }
-        }
-        .onChange(of: pickerItems) {
-            Task {
-                selectedImages.removeAll()
-
-                for item in pickerItems {
-                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
-                        selectedImages.append(loadedImage)
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Import a photo to get started"))
                     }
                 }
+                    Spacer()
+                    
+                    HStack {
+                        Text("Intensity")
+                        Slider(value: $filterIntensity)
+                    }
+                    .padding(.vertical)
+                    
+                    HStack {
+                        Button("Change Filter", action: changeFilter)
+                        // change filter
+                    }
+                    
+                    Spacer()
+                    
+                    // share the picture
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
             }
+            .onChange(of: selectedItem, loadImage)
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
     }
 }
