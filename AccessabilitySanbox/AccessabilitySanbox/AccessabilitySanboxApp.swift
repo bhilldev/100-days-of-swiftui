7d23419c87154816578a05535d20baacb014cce8
Brandon Hill <bhilldev@outlook.com>
2026-08-15 11:09:20 -0500

    Complete Day 74: Accessibility

---
+++ AccessabilitySanbox/AccessabilitySanbox/AccessabilitySanboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessabilitySanboxApp.swift
+//  AccessabilitySanbox
+//
+//  Created by Brandon Hill on 8/15/26.
+//
+
+import SwiftUI
+
+@main
+struct AccessabilitySanboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
