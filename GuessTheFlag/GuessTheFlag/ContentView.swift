c39d6523c826c51dbd2d2ab617e73d2de74c4d67
Brandon Hill <bhilldev@outlook.com>
2026-08-16 11:02:02 -0500

    Complete Day 75: Adding accessibility to existing projects

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -24,6 +24,20 @@ struct ContentView: View {
         "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"
     ].shuffled()
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var showingScore = false
@@ -78,6 +92,7 @@ struct ContentView: View {
                             
                         } label: {
                             FlagImage(country: countries[number])
+                                .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                         }
                         .rotation3DEffect(
                             .degrees(selectedFlag == number ? animationAmount : 0),
