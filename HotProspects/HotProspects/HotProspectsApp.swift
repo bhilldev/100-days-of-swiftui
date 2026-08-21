be4b259a1df625e9b8766e58929a220fdc5f32e0
Brandon Hill <bhilldev@outlook.com>
2026-08-21 12:00:35 -0500

    Complete Day  79: Creating tabs with TabView and tabitem()

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Brandon Hill on 8/21/26.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
