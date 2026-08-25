73949e575b98128fe193e5dd5fd407a234d134a0
Brandon Hill <bhilldev@outlook.com>
2026-08-25 12:45:49 -0500

    Day 83: Adding QR codes to HotProspects

--- HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -7,16 +7,18 @@
 
 import SwiftUI
 import SwiftData
+import CodeScanner
 
 enum FilterType {
     case none, contacted, uncontacted
 }
 
 struct ProspectsView: View {
-    
     let filter: FilterType
     @Query(sort: \Prospect.name) var prospects: [Prospect]
     @Environment(\.modelContext) var modelContext
+    @State private var isShowingScanner = false
+    @State private var selectedProspects = Set<Prospect>()
     
     init(filter: FilterType) {
         self.filter = filter
@@ -29,7 +31,6 @@ struct ProspectsView: View {
             }, sort: [SortDescriptor(\Prospect.name)])
         }
     }
-
     
     var title: String {
         switch filter {
@@ -41,16 +42,62 @@ struct ProspectsView: View {
             "Uncontacted people"
         }
     }
+    
     var body: some View {
         NavigationStack {
-            List(prospects) { prospect in
+            List(prospects, selection: $selectedProspects) { prospect in
                 VStack(alignment: .leading) {
                     Text(prospect.name)
                         .font(.headline)
                     Text(prospect.emailAddress)
                         .foregroundStyle(.secondary)
                 }
+                .swipeActions {
+                    Button("Delete", systemImage: "trash", role: .destructive) {
+                        modelContext.delete(prospect)
+                    }
+                    
+                    if prospect.isContacted {
+                        Button("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.blue)
+                    } else {
+                        Button("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.green)
+                    }
+                }
+                .tag(prospect)
             }
+            .navigationTitle(title)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Scan", systemImage: "qrcode.viewfinder") {
+                        isShowingScanner = true
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                
+                if selectedProspects.isEmpty == false {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button("Delete Selected", action: delete)
+                    }
+                }
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                Text("Scanner View Here")
+            }
+        }
+    }
+    
+    func delete() {
+        for prospect in selectedProspects {
+            modelContext.delete(prospect)
         }
     }
 }
