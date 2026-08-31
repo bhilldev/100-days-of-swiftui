39e51e77098e739a99974c8d71989d8c3e640d56
Brandon Hill <bhilldev@outlook.com>
2026-08-31 11:14:11 -0500

    Day 86: How to use gestures

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Brandon Hill on 8/31/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
