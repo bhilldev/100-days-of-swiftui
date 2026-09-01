2422f0d55e9a853c5fbfa07c9855a9875fb7200b
Brandon Hill <bhilldev@outlook.com>
2026-09-01 11:24:24 -0500

    Day 87: Accessibility, differentiating without color

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -9,19 +9,20 @@ import SwiftUI
 import Combine
 
 struct ContentView: View {
-    @Environment(\.scenePhase) var scenePhase
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
 
     var body: some View {
-        Text("Hello, world!")
-            .onChange(of: scenePhase) { oldPhase, newPhase in
-                if newPhase == .active {
-                    print("Active")
-                } else if newPhase == .inactive {
-                    print("Inactive")
-                } else if newPhase == .background {
-                    print("Background")
-                }
+        HStack {
+            if differentiateWithoutColor {
+                Image(systemName: "checkmark.circle")
             }
+
+            Text("Success")
+        }
+        .padding()
+        .background(differentiateWithoutColor ? .black : .green)
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
     }
 }
 #Preview {
