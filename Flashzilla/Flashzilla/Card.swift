aa04f550bf7295c3bd3b5e6d9c7b6280ad15d625
Brandon Hill <bhilldev@outlook.com>
2026-09-03 11:34:21 -0500

    Day 88: Moving views with DragGesture and offset()

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Brandon Hill on 9/3/26.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -6,30 +6,44 @@
 //
 
 import SwiftUI
-import Combine
 
 struct ContentView: View {
-    @Environment(\.accessibilityReduceMotion) var reduceMotion
-    @State private var scale = 1.0
-    
-    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-        if UIAccessibility.isReduceMotionEnabled {
-            return try body()
-        } else {
-            return try withAnimation(animation, body)
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+
+    var body: some View {
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
         }
     }
-    
-    var body: some View {
-        Button("Hello, World!") {
-            withOptionalAnimation {
-                scale *= 1.5
-            }
-            
-        }
-        .scaleEffect(scale)
+
+    func removeCard(at index: Int) {
+        guard index >= 0 && index < cards.count else { return }
+        cards.remove(at: index)
     }
 }
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
+
 #Preview {
     ContentView()
 }
