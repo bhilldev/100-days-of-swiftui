86f43681250031acc9ffbbdf7282441f2889b8c0
Brandon Hill <bhilldev@outlook.com>
2026-09-04 10:14:48 -0500

    Day 89: Counting down with a Timer

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     @State private var cards = Array<Card>(repeating: .example, count: 10)
     @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
 
 
     var body: some View {
@@ -19,6 +24,13 @@ struct ContentView: View {
                 .ignoresSafeArea()
 
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -28,6 +40,21 @@ struct ContentView: View {
                         }
                         .stacked(at: index, in: cards.count)
                     }
+                }
+                
+            }
+            .onReceive(timer) { time in
+                guard isActive else { return }
+
+                if timeRemaining > 0 {
+                    timeRemaining -= 1
+                }
+            }
+            .onChange(of: scenePhase) {
+                if scenePhase == .active {
+                    isActive = true
+                } else {
+                    isActive = false
                 }
             }
             if accessibilityDifferentiateWithoutColor {
