ab178ae1e7d3528bdb78232ec370055f365a69ef
Brandon Hill <bhilldev@outlook.com>
2026-09-14 11:08:23 -0500

    Day 92: Alignment and alignment guides

---
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Brandon Hill on 9/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { _ in Double(position) * -10 }
+            }
+        }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Brandon Hill on 9/14/26.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
