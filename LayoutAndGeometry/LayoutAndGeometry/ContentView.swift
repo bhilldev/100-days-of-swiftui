261c7dee6140ca57a36ec0e97caa8bf12eeb1800
Brandon Hill <bhilldev@outlook.com>
2026-09-15 11:11:15 -0500

    Day 93: Resizing images to fit the screen using GeometryReader

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .offset(x: 100, y: 100)
-            .background(.red)
+        GeometryReader { proxy in
+            Image(.example)
+                .resizable()
+                .scaledToFit()
+                .frame(width: proxy.size.width * 0.8)
+                .frame(width: proxy.size.width, height: proxy.size.height)
+        }
     }
 }
 
